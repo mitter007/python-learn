c8ba771a919e8bfd883227bc3e650b29875da9a8
JiaoWT <jiaowt762@163.com>
2025-04-01 17:18:44 +0800

    day01

---
+++ 02_Python入门语法/test01.py
@@ -0,0 +1,15 @@
+money=50
+print("当前钱包余额为：",money,"元")
+ice=10
+cola=5
+print("购买了冰淇淋花费了：",ice,"元")
+print("购买了可乐花费了：",cola,"元")
+money=money-ice-cola-0.555
+print("最终钱包还剩：",money,"元")
+print(type(money))
+
+type=type(money)
+
+print(type)
+x="33"
+print(money-x)
